c645d63d8a9918a20cdea5b96f7603690d3667f3
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-02-15 06:09:16 +0000

    new tests, OK
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/python@1831 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

---
+++ protocolIdentProtocol.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+'''
+OpenLCB ProtocolIdentificationProtocol message
+
+@author: Bob Jacobsen
+'''
+
+import connection as connection
+import canolcbutils
+
+def makeframe(alias, dest) :
+    body = [0x2E]
+    return canolcbutils.makeframestring(0x1E000000+alias+(dest<<12),body)
+    
+def usage() :
+    print ""
+    print "Called standalone, will send one CAN ProtocolIdentificationProtocol (addressed) message"
+    print " and display response"
+    print ""
+    print "Default connection detail taken from connection.py"
+    print ""
+    print "-a --alias source alias (default 0x"+hex(connection.thisNodeAlias).upper()+")"
+    print "-d --dest dest alias (default 0x"+hex(connection.testNodeAlias).upper()+")"
+    print "-n --node dest nodeID (default None, format 01.02.03.04.05.06)"
+    print "-t find destination alias and NodeID automatically"
+    print "-v verbose"
+    print "-V Very verbose"
+
+import getopt, sys
+
+def main():
+    # argument processing
+    alias = connection.thisNodeAlias
+    dest = connection.testNodeAlias
+    identifynode = False
+    verbose = False
+    
+    try:
+        opts, remainder = getopt.getopt(sys.argv[1:], "d:a:vVt", ["alias=", "dest="])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-v":
+            verbose = True
+        elif opt == "-V":
+            connection.network.verbose = True
+            verbose = True
+        elif opt in ("-a", "--alias"): # needs hex processing
+            alias = int(arg)
+        elif opt in ("-d", "--dest"): # needs hex processing
+            dest = int(arg)
+        elif opt == "-t":
+            identifynode = True
+        else:
+            assert False, "unhandled option"
+
+    if identifynode :
+        import getUnderTestAlias
+        dest, otherNodeId = getUnderTestAlias.get(alias, None)
+
+    retval = test(alias, dest, connection, verbose)
+    exit(retval)
+    
+def test(alias, dest, connection, verbose) :
+    if verbose : print "ProtocolIdentificationProtocol check OK"
+    connection.network.send(makeframe(alias, dest))
+    reply = connection.network.receive()
+    if (reply == None ) : 
+        if verbose : print "Expected reply not received"
+        return 2
+    if not (reply.startswith(":X1E") and int(reply[4:7],16)==alias and int(reply[7:10],16)==dest and reply[11:13]=="2F") :
+        if verbose : print "Unexpected reply received ", reply
+        return 1
+
+    if verbose : print "ProtocolIdentificationProtocol not addressed, no reply"
+    connection.network.send(makeframe(alias, (~dest)&0xFFF))
+    reply = connection.network.receive()
+    if (reply == None ) : 
+        return 0
+    else :
+        if verbose : print "Unexpected reply received ", reply
+        return 1
+
+    return 0
+
+if __name__ == '__main__':
+    main()
