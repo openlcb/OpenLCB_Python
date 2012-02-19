6b5ca6225ac481cfe45e0ab50013f9a60aee8c28
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-02-19 02:34:59 +0000

    new test for simple config info
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/python@1853 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- simpleNodeIdentificationInformation.py
+++ simpleNodeIdentificationInformation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 '''
-Check that unknown (unallocated) MTIs get a reject message
-
-Tests that implementor has not grabbed MTIs for their own purposes
+OpenLCB SimpleNodeIdentificantInformation message
 
 @author: Bob Jacobsen
 '''
@@ -10,30 +8,24 @@ Tests that implementor has not grabbed MTIs for their own purposes
 import connection as connection
 import canolcbutils
 
-def makeframe(alias, dest, mti) :
-    return canolcbutils.makeframestring(0x1E000000+alias+(dest<<12),[mti])
+def makeframe(alias, dest) :
+    body = [0x52]
+    return canolcbutils.makeframestring(0x1E000000+alias+(dest<<12),body)
     
 def usage() :
     print ""
-    print "Called standalone, will send one CAN message with unknown MTI"
+    print "Called standalone, will send one CAN Simple Node Identificant Information (addressed) message"
     print " and display response"
-    print ""
-    print "Expect a single error reply in return"
-    print "e.g. [1Edddsss] 0C nn nn nn nn nn nn"
-    print "containing dest alias and error info"
     print ""
     print "Default connection detail taken from connection.py"
     print ""
     print "-a --alias source alias (default 0x"+hex(connection.thisNodeAlias).upper()+")"
     print "-d --dest dest alias (default 0x"+hex(connection.testNodeAlias).upper()+")"
-    print "-n --node dest nodeID (default 01.02.03.04.05.06)"
     print "-t find destination alias automatically"
     print "-v verbose"
     print "-V Very verbose"
 
 import getopt, sys
-
-knownMti = [0x0A, 0x0C, 0x0D, 0x2B, 0x2E, 0x2F, 0x4C, 0x4D, 0x4E, 0x4F, 0x52, 0x53, 0x6A, 0x6B]
 
 def main():
     # argument processing
@@ -66,23 +58,34 @@ def main():
 
     if identifynode :
         import getUnderTestAlias
-        dest, nodeID = getUnderTestAlias.get(alias, None)
+        dest, otherNodeId = getUnderTestAlias.get(alias, None)
 
     retval = test(alias, dest, connection, verbose)
     exit(retval)
     
 def test(alias, dest, connection, verbose) :
-    for mti in range(0,256) :
-        if mti in knownMti : continue
-        connection.network.send(makeframe(alias, dest, mti))
+    if verbose : print "SimpleNodeIdentificantInformation check valid request" 
+    connection.network.send(makeframe(alias, dest))
+    reply = connection.network.receive()
+    if reply == None : 
+        print "Expected reply not received"
+        return 2
+    while (True) :
         reply = connection.network.receive()
-        if reply == None : 
-            print "Expected reply not received for", mti
-            return 2
-        if  (not reply.startswith(":X1E")) or reply[11:13] != "0C"  : 
-            print "Wrong reply received for", mti
-            return 4
-    return 0
+        if reply == None :
+            break
+        if not (reply.startswith(":X1E") and int(reply[4:7],16)==alias and int(reply[7:10],16)==dest and reply[11:13]=="53") :
+            print "Unexpected reply received ", reply
+            return 1
+
+    if verbose : print "SimpleNodeIdentificantInformation address other node, expect no reply"
+    connection.network.send(makeframe(alias, (~dest)&0xFFF))
+    reply = connection.network.receive()
+    if reply == None : 
+        return 0
+    else :
+        print "Unexpected reply received ", reply
+        
 
 if __name__ == '__main__':
     main()
