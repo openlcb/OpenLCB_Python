055b2a66113d7036f8e71ebda13b5620d267d7b3
Don Goodman-Wilson <don@screenhero.com>
2012-02-22 21:11:48 +0000

    Added errorcode to NAK to elicit retransmission of datagram. Need to add a permanent failure NAK test too.
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/python@1913 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- testDatagram.py
+++ testDatagram.py
@@ -165,7 +165,7 @@ def test(alias, dest, connection, verbose) :
         print "Unexpected message instead of datagram segment", reply
         return 33
     # send NAK for retransmit and see if it's right this time
-    connection.network.send(canolcbutils.makeframestring(0x1E000000+alias+(dest<<12),[0x4D]))
+    connection.network.send(canolcbutils.makeframestring(0x1E000000+alias+(dest<<12),[0x4D,02,00]))
     retval = datagram.receiveOneDatagram(alias, dest, connection, verbose)
     if type(retval) is int : 
         # pass error code up
