71f2512a979f58fec535a94d4216d444da78b52d
Bob Jacobsen <jacobsen@mac.com>
2012-03-04 15:02:47 +0000

    much more decoding; display all address spaces (too long now); clean up indents
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/python@1965 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- testConfigurationProtocol.py
+++ testConfigurationProtocol.py
@@ -75,12 +75,27 @@ def test(alias, dest, connection, verbose) :
         print "Unexpected message instead of read reply datagram ", retval
         return 3
     if verbose : 
-        print "Configuration Options:"
-        print "   Available commands ", hex(retval[2]*256+retval[3])
-        print "        Write lengths ", hex(retval[4])
-        print "        Highest space ", retval[5]
-        print "         Lowest space ", retval[6]
-
+        print "  Configuration Options:"
+        cmd = retval[2]*256+retval[3]
+        print "    Available commands (",hex(cmd),")"
+        print "                Write under mask: ", "yes" if cmd&0x8000 else "no"
+        print "                 Unaligned reads: ", "yes" if cmd&0x4000 else "no"
+        print "                Unaligned writes: ", "yes" if cmd&0x2000 else "no"
+        print "              Mfg ACDI 0xFD read: ", "yes" if cmd&0x0800 else "no"
+        print "              Usr ACDI 0xFC read: ", "yes" if cmd&0x0400 else "no"
+        print "             Usr ACDI 0xFC write: ", "yes" if cmd&0x0200 else "no"
+        print "    Write lengths (", hex(retval[4]),")"
+        print "                          1 byte: ", "yes" if retval[4]&0x80 else "no"
+        print "                          2 byte: ", "yes" if retval[4]&0x40 else "no"
+        print "                          4 byte: ", "yes" if retval[4]&0x20 else "no"
+        print "                         64 byte: ", "yes" if retval[4]&0x10 else "no"
+        print "                       arbitrary: ", "yes" if retval[4]&0x02 else "no"
+        print "                          stream: ", "yes" if retval[4]&0x01 else "no"
+        print "    Highest space ", retval[5]
+        print "    Lowest space  ", retval[6]
+    lowSpace = retval[6]
+    highSpace = retval[5]
+    
     # One byte read from config space
     retval = datagram.sendOneDatagram(alias, dest, [0x20,0x42,0,0,0,0,1], connection, verbose)
     if retval != 0 :
@@ -93,7 +108,7 @@ def test(alias, dest, connection, verbose) :
     if retval[0:6] != [0x20,0x52,0x00,0x00,0x00,0x00] :
         print "Unexpected message instead of read reply datagram ", retval
         return 3
-    if verbose : print "read value", retval[6:7]
+    if verbose : print "  Read one byte result", retval[6:7]
     
     # Eight byte read from config space
     retval = datagram.sendOneDatagram(alias, dest, [0x20,0x42,0,0,0,0,8], connection, verbose)
@@ -107,32 +122,37 @@ def test(alias, dest, connection, verbose) :
     if retval[0:6] != [0x20,0x52,0x00,0x00,0x00,0x00] :
         print "Unexpected message instead of read reply datagram ", retval
         return 3
-    if verbose : print "read value", retval[6:]
+    if verbose : print "  Read eight bytes result", retval[6:]
+       
+    n = highSpace
+    while n >= lowSpace-1 :
+        # Get Address Space Info from each space
+        retval = datagram.sendOneDatagram(alias, dest, [0x20,0x84,n], connection, verbose)
+        if retval != 0 :
+            return retval
+        # read data response
+        retval = datagram.receiveOneDatagram(alias, dest, connection, verbose)
+        if (type(retval) is int) : 
+            # pass error code up
+            return retval
+        if retval[0] != 0x20 or retval[1]&0xFE != 0x86 or retval[2] != n :
+            print "Unexpected message instead of starting with [0x20,0x86,",n,"] read reply datagram: ", retval
+            return 3
+        if verbose : 
+            print "  Address Space",n,"Options:"
+            print "      Present? ", "yes" if retval[1]&0x01 else "no"
+            print "      Highest address ", hex(((retval[3]*256+retval[4])*256+retval[5])*256+retval[6])
+            print "                Flags (", hex(retval[7]),")"
+            print "                        Read-only ", "yes" if retval[7]&0x01 else "no"
+            
+            if len(retval) > 9:
+                print "      Lowest address ", hex(((retval[9]*256+retval[10])*256+retval[11])*256+retval[12])
+            else:
+                print "      Lowest address implied at 0x0000"
+            if len(retval) > 12:
+                print "           Space name ", str(retval[12:])
+        n = n-1
         
-    # Get Address Space Info from space 0xFF
-    retval = datagram.sendOneDatagram(alias, dest, [0x20,0x84,0xFF], connection, verbose)
-    if retval != 0 :
-        return retval
-    # read data response
-    retval = datagram.receiveOneDatagram(alias, dest, connection, verbose)
-    if (type(retval) is int) : 
-        # pass error code up
-        return retval
-    if retval[0:4] != [0x20,0x86,0x01,0xFF] :
-        print "Unexpected message instead of read reply datagram ", retval
-        return 3
-    if verbose : 
-        print "Address Space Options:"
-        print "      Present? ", hex(retval[2])
-        print "      Highest address ", hex(((retval[4]*256+retval[5])*256+retval[6])*256+retval[7])
-        print "      Alignment flags ", hex(retval[8])
-        if len(retval) > 9:
- 		print "      Lowest address ", hex(((retval[9]*256+retval[10])*256+retval[11])*256+retval[12])
-	else:
- 		print "      Lowest address 0x0000"
-	if len(retval) > 12:
-	        print "           Space name ", str(retval[12:])
-    
     return 0
 
 if __name__ == '__main__':
