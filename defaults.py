081b7bdaa70c02992bc4aa9ffc1c9e90f56f65e0
jacobsen <jacobsen@87536cb5-86f1-4cdc-8fc6-3f0f71bcd267>
2012-03-06 05:14:38 +0000

    add local
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/python@1981 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- defaults.py
+++ defaults.py
@@ -1,14 +1,24 @@
-import ethernetolcblink
-network = ethernetolcblink.EthernetToOlcbLink()
-network.host = "10.00.01.98"
-#network.host = "99.32.118.100"
-network.port = 23
 
-# import serialolcblink
-# network = serialolcblink.SerialOlcbLink()
-# network.host = "/dev/tty.usbserial-A7007AOK"
-# network.port = 115200
-# network.startdelay = 4
+serial = True
+ethernet = not serial
+
+local = False
+
+if ethernet :
+    import ethernetolcblink
+    network = ethernetolcblink.EthernetToOlcbLink()
+    network.host = "10.00.01.98"
+    #network.host = "99.32.118.100"
+    network.port = 23
+elif serial : 
+    import serialolcblink
+    network = serialolcblink.SerialOlcbLink()
+    network.port = "/dev/tty.usbserial-A6008c44"
+    network.speed = 230400
+    network.startdelay = 1
+elif local :
+    import pipeolcblink
+    network = pipeolcblink.PipeOlcbLink()
 
 
 thisNodeID = [1,2,3,4,5,6]
