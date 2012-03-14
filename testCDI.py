e46906219b692636881fca06a093de64823be289
Bob Jacobsen <jacobsen@mac.com>
2012-03-14 00:15:24 +0000

    now uses xmllint to check the CDI is well formed
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/python@2006 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- testCDI.py
+++ testCDI.py
@@ -61,21 +61,51 @@ def main():
     
 def test(alias, dest, connection, verbose) :
 
-    # Read from CDI space
-    retval = datagram.sendOneDatagram(alias, dest, [0x20,0x43,0,0,0,0,8], connection, verbose)
-    if retval != 0 :
-        return retval
-    # read data response
-    retval = datagram.receiveOneDatagram(alias, dest, connection, verbose)
-    if (type(retval) is int) : 
-        # pass error code up
-        return retval
-    if retval[0:6] != [0x20,0x53,0x00,0x00,0x00,0x00] :
-        print "Unexpected message instead of read reply datagram ", retval
-        return 3
-    if verbose : print "  Read CDI result", chr(retval[6]),chr(retval[7]),chr(retval[8])
-               
-    return 0
+    # instead of checking the length of the address space (not cleanly required anyway)
+    # this assumes a null-terminated string and reads until it gets the null
+
+    address = 0
+    result = ""
+    chunk = 16
+    done = False
+    
+    while True :    
+        # Read from CDI space
+        retval = datagram.sendOneDatagram(alias, dest, [0x20,0x43,(address>>24)&0xFF,(address>>16)&0xFF,(address>>8)&0xFF,address&0xFF,chunk], connection, verbose)
+        if retval != 0 :
+            return retval
+        # read data response
+        retval = datagram.receiveOneDatagram(alias, dest, connection, verbose)
+        if (type(retval) is int) : 
+            # pass error code up
+            return retval
+        if retval[0:2] != [0x20,0x53] :
+            print "Unexpected message instead of read reply datagram ", retval
+            return 3
+        for c in retval[6:] :
+            result = result+chr(c)
+            if c == 0 :
+                done = True
+                break;
+        if done : break
+        address = address + chunk
+        
+    if verbose : print "   Read CDI result was ", len(result), " bytes"
+    if connection.network.verbose : print "  Read CDI result ++++++++++\n", result,"\n++++++++++++++++"
+        
+    executable = "xmllint --noout -schema ../xml/schema/cdi.xsd - "
+
+    import subprocess
+    process = subprocess.Popen(executable,1,None,subprocess.PIPE,subprocess.PIPE, 
+                    subprocess.STDOUT, None, False, True)
+    [stdout, stderr] = process.communicate(result) 
+    
+    if process.returncode != 0 :
+        print "   CDI data did not validate:  ", stdout
+    if process.returncode == 0 and verbose :
+        print "   CDI result check:  ", stdout
+
+    return process.returncode
 
 if __name__ == '__main__':
     main()
