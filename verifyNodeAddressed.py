24c0ef8e3b3efa6d191b84f4d7aa34c2f5226a79
Stuart Baker <stuart_w_baker@ymail.com>
2016-10-01 18:30:37 -0500

    Cleanup VerifyNodeAddressed tests.

--- verifyNodeAddressed.py
+++ verifyNodeAddressed.py
@@ -13,118 +13,105 @@ def makeframe(alias, dest, nodeID) :
     body = [(dest>>8)&0xFF, dest&0xFF]
     if nodeID != None : body = body+nodeID
     return canolcbutils.makeframestring(0x19488000+alias,body)
-    
-def usage() :
-    print ""
-    print "Called standalone, will send one CAN VerifyNode (addressed) message"
-    print " and display response"
-    print ""
-    print "Expect a single VerifiedNode reply in return"
-    print "e.g. [180B7sss] nn nn nn nn nn nn"
-    print "containing dest alias and NodeID"
-    print ""
-    print "Default connection detail taken from connection.py"
-    print ""
-    print "-a --alias source alias (default 0x"+hex(connection.thisNodeAlias).upper()+")"
-    print "-d --dest dest alias (default 0x"+hex(connection.testNodeAlias).upper()+")"
-    print "-n --node dest nodeID (default None, format 01.02.03.04.05.06)"
-    print "-t find destination alias and NodeID automatically"
-    print "-v verbose"
-    print "-V Very verbose"
 
-import getopt, sys
+from optparse import OptionParser
 
 def main():
     # argument processing
-    nodeID = None
-    alias = connection.thisNodeAlias
-    dest = connection.testNodeAlias
-    identifynode = False
-    verbose = False
+    usage = "usage: %prog [options]\n\n" + \
+            "Called standalone, will send one CAN VerifyNode (addressed) " + \
+            "message.\n\n" + \
+            "Expect a single VerifiedNode reply in return\n" + \
+            "  e.g. [180B7sss] nn nn nn nn nn nn\n" + \
+            "containing dest alias and NodeID\n\n" + \
+            "valid usages (default values):\n" + \
+            "  ./verifyNodeAddressed.py\n" + \
+            "  ./verifyNodeAddressed.py -a 0xAAA\n" + \
+            "  ./verifyNodeAddressed.py -a 0xAAA -d 0x5F9\n" + \
+            "  ./verifyNodeAddressed.py -a 0xAAA -d 0x5F9 " + \
+            "-n 0x2 0x1 0x99 0xff 0x00 0x1e\n\n" + \
+            "Default connection detail taken from connection.py"
+
+    parser = OptionParser(usage=usage)
+    parser.add_option("-a", "--alias", dest="alias", metavar="ALIAS",
+                      default=connection.thisNodeAlias, type = int,
+                      help="source alias")
+    parser.add_option("-d", "--dest", dest="dest", metavar="ALIAS",
+                      default=connection.testNodeAlias, type = int,
+                      help="destination alias")
+    parser.add_option("-n", "--node", dest="nodeid",
+                      metavar="0x1 0x2 0x3 0x4 0x5 0x6",
+                      default=connection.testNodeID, type=int, nargs=6,
+                      help="destination Node ID")
+    parser.add_option("-t", "--auto", action="store_true", dest="identifynode",
+                      default=False,
+                      help="find destination NodeID automatically")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                      default=False,
+                      help="print verbose debug information")
+    parser.add_option("-V", "--veryverbose",
+                      action="store_true", dest="veryverbose",
+                      default=False,
+                      help="print very verbose debug information")
+
+    (options, args) = parser.parse_args()
+
+    if options.veryverbose :
+        connection.network.verbose = True
     
-    try:
-        opts, remainder = getopt.getopt(sys.argv[1:], "d:n:a:vVt", ["alias=", "node=", "dest="])
-    except getopt.GetoptError, err:
-        # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
-        usage()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == "-v":
-            verbose = True
-        elif opt == "-V":
-            connection.network.verbose = True
-            verbose = True
-        elif opt in ("-a", "--alias"): # needs hex processing
-            alias = int(arg)
-        elif opt in ("-d", "--dest"): # needs hex processing
-            dest = int(arg)
-        elif opt in ("-n", "--node"):
-            nodeID = canolcbutils.splitSequence(arg)
-        elif opt == "-t":
-            identifynode = True
-        else:
-            assert False, "unhandled option"
+    '''
+    @todo identifynode option not currently implemented
+    '''
+    #if identifynode :
+    #    import getUnderTestAlias
+    #    dest, otherNodeId = getUnderTestAlias.get(alias, None, verbose)
+    #    if nodeID == None : nodeID = otherNodeId
 
-    if identifynode :
-        import getUnderTestAlias
-        dest, otherNodeId = getUnderTestAlias.get(alias, None, verbose)
-        if nodeID == None : nodeID = otherNodeId
-
-    retval = test(alias, dest, nodeID, connection, verbose)
+    retval = test(options.alias, options.dest, options.nodeid, connection,
+                  options.verbose)
     connection.network.close()
     exit(retval)
     
 def test(alias, dest, nodeID, connection, verbose) :
     # send correct address, correct node ID in body
     connection.network.send(makeframe(alias, dest, nodeID))
-    reply = connection.network.receive()
-    if (reply == None ) : 
+    expect = canolcbutils.makeframestring(0x19170000 + dest, nodeID)
+    if (connection.network.expect(exact=expect) == None) :
         print "Expected reply to correct alias & correct ID not received"
         return 2
-    elif not reply.startswith(":X19170") :
-        print "Unexpected reply received ", reply
-        return 1
 
     # send correct address, no node ID in body
     connection.network.send(makeframe(alias, dest, None))
-    reply = connection.network.receive()
-    if (reply == None ) : 
+    if (connection.network.expect(startswith=":X19170", data=nodeID) == None) :
         print "Expected reply to correct alias & no ID not received"
         return 2
-    elif not reply.startswith(":X19170") :
-        print "Unexpected reply received ", reply
-        return 1
 
     # send correct address, wrong node ID in body
     tnodeID = copy.copy(nodeID)
     tnodeID[0] = tnodeID[0]^1
     
     connection.network.send(makeframe(alias, dest, tnodeID))
-    reply = connection.network.receive()
-    if (reply == None ) : 
+    if (connection.network.expect(startswith=":X19170", data=nodeID) == None) :
         print "Expected reply to correct alias & incorrect ID not received"
         return 2
-    elif not reply.startswith(":X19170") :
-        print "Unexpected reply received ", reply
-        return 1
 
     # repeat all three with invalid alias
     connection.network.send(makeframe(alias, (~dest)&0xFFF, nodeID))
-    reply = connection.network.receive()
-    if (reply != None ) : 
+    expect = canolcbutils.makeframestring(0x19170000 + dest, nodeID)
+    reply = connection.network.expect(exact=expect)
+    if (reply != None) :
         print "Unexpected reply received on incorrect alias, OK nodeID", reply
         return 1
     
     connection.network.send(makeframe(alias, (~dest)&0xFFF, None))
-    reply = connection.network.receive()
-    if (reply != None ) : 
+    reply = connection.network.expect(startswith=":X19170", data=nodeID)
+    if (reply != None) :
         print "Unexpected reply received on incorrect alias, no nodeID", reply
         return 1
     
     connection.network.send(makeframe(alias, (~dest)&0xFFF, tnodeID))
-    reply = connection.network.receive()
-    if (reply != None ) : 
+    reply = connection.network.expect(startswith=":X19170", data=nodeID)
+    if (reply != None) :
         print "Unexpected reply received on incorrect alias, wrong nodeID", reply
         return 1
     
