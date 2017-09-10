3022896a21af8a6cd3a5cea6e4435b7080902779
Stuart Baker <stuart_w_baker@ymail.com>
2017-09-10 14:32:23 -0500

    Update test.

--- verifyNodeAddressed.py
+++ verifyNodeAddressed.py
@@ -6,84 +6,46 @@ OpenLCB VerifyNodeAddressed message
 @author: Stuart Baker - cleaned up and modernized
 '''
 
-import connection as connection
-import canolcbutils
+import connection
+import mtiDefs
+import optionsParsing
+
+import time
 import copy
 
-def makeframe(alias, dest, nodeID) :
-    body = [(dest>>8)&0xFF, dest&0xFF]
-    if nodeID != None : body = body+nodeID
-    return canolcbutils.makeframestring(0x19488000+alias,body)
+#def makeframe(alias, dest, nodeID) :
+#    body = [(dest>>8)&0xFF, dest&0xFF]
+#    if nodeID != None : body = body+nodeID
+#    return canolcbutils.makeframestring(0x19488000+alias,body)
 
-from optparse import OptionParser
-
+## run this test only
 def main():
     # argument processing
-    usage = "usage: %prog [options]\n\n" + \
-            "Called standalone, will send one CAN VerifyNode (addressed) " + \
-            "message.\n\n" + \
-            "Expect a single VerifiedNode reply in return\n" + \
-            "  e.g. [180B7sss] nn nn nn nn nn nn\n" + \
-            "containing dest alias and NodeID\n\n" + \
-            "valid usages (default values):\n" + \
-            "  ./verifyNodeAddressed.py\n" + \
-            "  ./verifyNodeAddressed.py -a 0xAAA\n" + \
-            "  ./verifyNodeAddressed.py -a 0xAAA -d 0x5F9\n" + \
-            "  ./verifyNodeAddressed.py -a 0xAAA -d 0x5F9 " + \
-            "-n 0x2 0x1 0x99 0xff 0x00 0x1e\n\n" + \
-            "Default connection detail taken from connection.py"
+    (alias, nodeID, connection, verbose, complete, repeat) = \
+        optionsParsing.parse("verifyNodeGobal")
 
-    parser = OptionParser(usage=usage)
-    parser.add_option("-a", "--alias", dest="alias", metavar="ALIAS",
-                      default=connection.thisNodeAlias, type = int,
-                      help="source alias")
-    parser.add_option("-d", "--dest", dest="dest", metavar="ALIAS",
-                      default=connection.testNodeAlias, type = int,
-                      help="destination alias")
-    parser.add_option("-n", "--node", dest="nodeid",
-                      metavar="0x1 0x2 0x3 0x4 0x5 0x6",
-                      default=connection.testNodeID, type=int, nargs=6,
-                      help="destination Node ID")
-    parser.add_option("-t", "--auto", action="store_true", dest="identifynode",
-                      default=False,
-                      help="find destination NodeID automatically")
-    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
-                      default=False,
-                      help="print verbose debug information")
-    parser.add_option("-V", "--veryverbose",
-                      action="store_true", dest="veryverbose",
-                      default=False,
-                      help="print very verbose debug information")
-
-    (options, args) = parser.parse_args()
-
-    if options.veryverbose :
-        connection.network.verbose = True
-    
-    '''
-    @todo identifynode option not currently implemented
-    '''
-    #if identifynode :
-    #    import getUnderTestAlias
-    #    dest, otherNodeId = getUnderTestAlias.get(alias, None, verbose)
-    #    if nodeID == None : nodeID = otherNodeId
-
-    retval = test(options.alias, options.dest, options.nodeid, connection,
-                  options.verbose)
+    # now execute
+    retval = test(nodeID, connection)
     connection.network.close()
-    exit(retval)
+    exit(retval)    
     
-def test(alias, dest, nodeID, connection, verbose) :
-    # send correct address, correct node ID in body
-    connection.network.send(makeframe(alias, dest, nodeID))
-    expect = canolcbutils.makeframestring(0x19170000 + dest, nodeID)
-    if (connection.network.expect(exact=expect) == None) :
+## run test
+# @param nodeID destination Node ID
+# @param connection OpenLCB connection
+def test(nodeID, connection) :
+    # send correct address, node ID in the body
+    connection.network.send(mtiDefs.OlcbMessage(mtiDefs.VERIFY_NODE_ID_ADDRESSED,
+                                                dest = nodeID, payload = nodeID))
+    if (connection.network.expect(mti = mtiDefs.VERIFY_NODE_ID_NUMBER,
+                                  source = nodeID, payload = nodeID) == None) :
         print "Expected reply to correct alias & correct ID not received"
         return 2
 
+    connection.network.send(mtiDefs.OlcbMessage(mtiDefs.VERIFY_NODE_ID_ADDRESSED,
+                                                dest = nodeID))
     # send correct address, no node ID in body
-    connection.network.send(makeframe(alias, dest, None))
-    if (connection.network.expect(startswith=":X19170", data=nodeID) == None) :
+    if (connection.network.expect(mti = mtiDefs.VERIFY_NODE_ID_NUMBER,
+                                  source = nodeID, payload = nodeID) == None) :
         print "Expected reply to correct alias & no ID not received"
         return 2
 
@@ -91,31 +53,43 @@ def test(alias, dest, nodeID, connection, verbose) :
     tnodeID = copy.copy(nodeID)
     tnodeID[0] = tnodeID[0]^1
     
-    connection.network.send(makeframe(alias, dest, tnodeID))
-    if (connection.network.expect(startswith=":X19170", data=nodeID) == None) :
+    connection.network.send(mtiDefs.OlcbMessage(mtiDefs.VERIFY_NODE_ID_ADDRESSED,
+                                                dest = nodeID, payload = tnodeID))
+    if (connection.network.expect(mti = mtiDefs.VERIFY_NODE_ID_NUMBER,
+                                  source = nodeID, payload = nodeID) == None) :
         print "Expected reply to correct alias & incorrect ID not received"
         return 2
 
-    # repeat all three with invalid alias
-    connection.network.send(makeframe(alias, (~dest)&0xFFF, nodeID))
-    expect = canolcbutils.makeframestring(0x19170000 + dest, nodeID)
-    reply = connection.network.expect(exact=expect)
+
+    # repeate all three with invalid node ID, use our own ID
+    invalidID = connection.network.get_node_id_self()
+    connection.network.send(mtiDefs.OlcbMessage(mtiDefs.VERIFY_NODE_ID_ADDRESSED,
+                                                dest = invalidID,
+                                                payload = nodeID))
+    reply = connection.network.expect(mti = mtiDefs.VERIFY_NODE_ID_NUMBER,
+                                  source = nodeID, payload = nodeID)
     if (reply != None) :
-        print "Unexpected reply received on incorrect alias, OK nodeID", reply
+        print "Unexpected reply received on incorrect Node ID, OK nodeID", reply
         return 1
     
-    connection.network.send(makeframe(alias, (~dest)&0xFFF, None))
-    reply = connection.network.expect(startswith=":X19170", data=nodeID)
+    connection.network.send(mtiDefs.OlcbMessage(mtiDefs.VERIFY_NODE_ID_ADDRESSED,
+                                                dest = invalidID))
+    reply = connection.network.expect(mti = mtiDefs.VERIFY_NODE_ID_NUMBER,
+                                      source = nodeID, payload = nodeID)
     if (reply != None) :
-        print "Unexpected reply received on incorrect alias, no nodeID", reply
+        print "Unexpected reply received on incorrect Node ID, no nodeID", reply
         return 1
     
-    connection.network.send(makeframe(alias, (~dest)&0xFFF, tnodeID))
-    reply = connection.network.expect(startswith=":X19170", data=nodeID)
+    connection.network.send(mtiDefs.OlcbMessage(mtiDefs.VERIFY_NODE_ID_ADDRESSED,
+                                                dest = invalidID,
+                                                payload = tnodeID))
+    reply = connection.network.expect(mti = mtiDefs.VERIFY_NODE_ID_NUMBER,
+                                      source = nodeID, payload = nodeID)
     if (reply != None) :
-        print "Unexpected reply received on incorrect alias, wrong nodeID", reply
+        print "Unexpected reply received on incorrect Node ID, wrong nodeID", \
+              reply
         return 1
-    
+
     return 0
 
 if __name__ == '__main__':
