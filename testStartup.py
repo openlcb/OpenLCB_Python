d9262e834c0b37e7a7ea75e3d5561cd5710e2039
Bob Jacobsen <jacobsen@mac.com>
2023-05-28 11:53:16 -0400

    p3; improve output

--- testStartup.py
+++ testStartup.py
@@ -26,18 +26,19 @@ import connection as connection
 import canolcbutils
 
 import time
-    
+import sys
+
 def usage() :
-    print ""
-    print "Called standalone, tests a nodes startup processing."
-    print "Reset the node (or force startup) after the script starts"
-    print ""
-    print "Default connection detail taken from connection.py"
-    print ""
-    print "-a --alias source alias"
-    print "-d --dest destination (target node) starting alias"
-    print "-v verbose"
-    print "-V very verbose"
+    print ("")
+    print ("Called standalone, tests a nodes startup processing.")
+    print ("Reset the node (or force startup) when prompted")
+    print ("")
+    print ("Default connection detail taken from connection.py")
+    print ("")
+    print ("-a --alias source alias")
+    print ("-d --dest destination (target node) starting alias")
+    print ("-v verbose")
+    print ("-V very verbose")
 
 import getopt, sys
 
@@ -45,13 +46,13 @@ def main():
     alias = connection.thisNodeAlias;
     dest = connection.testNodeAlias;
     verbose = False
-    
+
     # argument processing
     try:
         opts, remainder = getopt.getopt(sys.argv[1:], "d:a:vV", ["alias=", "dest="])
-    except getopt.GetoptError, err:
+    except getopt.GetoptError as err:
         # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
+        print (str(err)) # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
     for opt, arg in opts:
@@ -69,152 +70,161 @@ def main():
 
     retval = test(alias, dest, connection, verbose)
     connection.network.close()
-    return retval
-    
+    sys.exit(retval)
+
 def test(alias, dest, connection, verbose) :
     # wait for reset
-    
-    # expect RIF sequence (check timing)
+
+    # expect RIF sequence after reset, check timing
     timeout = connection.network.timeout
     connection.network.timeout = 25
     connection.network.connect()
-    if verbose : print "Restart node now"
+
+    print ("Restart node now")
+
     reply = connection.network.receive()
-    if verbose : print "Start checking node output"
+    if verbose : print ("Start checking node output")
     while (True) :
         if reply == None :
-            print "1st CIM reply not received, did you reset node?"
+            print ("1st CIM reply not received, did you reset node?")
             return 1
         elif reply.startswith(":X17") :
             break
         elif reply.startswith(":X") :
-            if verbose: print "ignoring unexpected frame", reply
+            if verbose: print ("ignoring unexpected frame", reply)
         else :
-            if verbose: print "ignoring misc characters not a frame: ", reply
+            if verbose: print ("ignoring misc characters not a frame: ", reply)
         reply = connection.network.receive()
-        
+
     connection.network.timeout = timeout
     testAlias = reply[7:10]
     if testAlias == "000" :
-        print "node using alias == 0"
+        print ("node using alias == 0")
         return 331
-        
+
     id = reply[4:7]
     start = time.time()
-    
+
     reply = connection.network.receive()
     if reply == None :
-        print "2nd CIM reply not received"
+        print ("2nd CIM reply not received")
         return 2
     if reply[7:10] != testAlias :
-        print "mismatched 2nd CIM source alias"
+        print ("mismatched 2nd CIM source alias")
         return 12
     if not reply.startswith(":X16") :
-        print "2nd CIM reply not correct"
+        print ("2nd CIM reply not correct")
         return 32
     id = id+reply[4:7]
 
     reply = connection.network.receive()
     if reply == None :
-        print "3rd CIM reply not received"
+        print ("3rd CIM reply not received")
         return 3
     if reply[7:10] != testAlias :
-        print "mismatched 3rd CIM source alias"
+        print ("mismatched 3rd CIM source alias")
         return 13
     if not reply.startswith(":X15") :
-        print "3rd CIM reply not correct"
+        print ("3rd CIM reply not correct")
         return 33
     id = id+reply[4:7]
 
     reply = connection.network.receive()
     if reply == None :
-        print "4th CIM reply not received"
-        return 4
+        print ("4th CIM reply not received")
+        return (4)
     if reply[7:10] != testAlias :
-        print "mismatched 4th CIM source alias"
-        return 14
+        print ("mismatched 4th CIM source alias")
+        return (14)
     if not reply.startswith(":X14") :
-        print "4th CIM reply not correct"
-        return 34
+        print ("4th CIM reply not correct")
+        return (34)
     id = id+reply[4:7]
-    
-    # expect CIF (check timing)
-    connection.network.timeout = 1 
+
+    # expect RIM (check timing)
+    connection.network.timeout = 1
     reply = connection.network.receive()
     end = time.time()
     connection.network.timeout = timeout
     if reply == None :
-        print "RIM reply not received"
-        return 5
+        print ("RIM reply not received")
+        return (5)
     if not reply.startswith(":X10700") :
-        print "RIM reply not correct"
-        return 35
+        print ("RIM reply not correct")
+        return (35)
     if reply[7:10] != testAlias :
-        print "mismatched RIM source alias"
-        return 15
+        print ("mismatched RIM source alias")
+        return (15)
 
-    if verbose : print "delay was ", end-start
+    if verbose : print ("   delay was ", end-start)
 
     if end-start < 0.15 :
         # some tolerance on check...
-        print "did not wait long enough ", end-start
-        return 22
+        print ("did not wait long enough ", end-start)
+        return (22)
     if end-start > .55 :
         # some tolerance on check...
-        print "waited too long ", end-start
-        return 22
+        print ("waited too long ", end-start)
+        return (22)
 
     # expect AMD
     reply = connection.network.receive()
     if reply == None :
-        print "AMD reply not received"
-        return 6
+        print ("AMD reply not received")
+        return (6)
     if not reply.startswith(":X10701") :
-        print "AMD reply not correct"
-        return 35
+        print ("AMD reply not correct")
+        return (35)
     if reply[7:10] != testAlias :
-        print "mismatched AMD source alias"
-        return 16
+        print ("mismatched AMD source alias")
+        return (16)
     if id != reply[11:23] :
-        print "AMD node ID ",reply[11:23]," did not match one in CID frames ",id
-        return 21
-    
+        print ("AMD node ID ",reply[11:23],") did not match one in CID frames ",id)
+        return (21)
+
     # expect NodeInit
     reply = connection.network.receive()
     if reply == None :
-        print "NodeInit reply not received"
+        print ("NodeInit reply not received")
         return 7
     if not reply.startswith(":X19100") :
-        print "NodeInit reply not correct"
+        print ("NodeInit reply not correct")
         return 37
     if reply[7:10] != testAlias :
-        print "mismatched NodeInit source alias"
+        print ("mismatched NodeInit source alias")
         return 17
     if id != reply[11:23] :
-        print "NodeInit node ID did not match",id, reply[11:23]
-        return 27
-    
-    # expect one or more Produced/Consumed messages
+        print ("NodeInit node ID did not match",id, reply[11:23])
+        return (27)
+
+    # expect zero or more Produced/Consumed messages
 
     consumed = []
     produced = []
-    while (True) :
+    start = time.time()
+    while (time.time() - start < 5.0) :
         reply = connection.network.receive()
         if (reply == None ) : break
         if (reply.startswith(":X194C")) :
             event = canolcbutils.bodyArray(reply)
-            if verbose : print "consumes ", event
+            if verbose : print ("consumes ", event)
             consumed = consumed+[event]
         elif (reply.startswith(":X1954")) :
             event = canolcbutils.bodyArray(reply)
-            if verbose : print "produces ", event
+            if verbose : print ("produces ", event)
             produced = produced+[event]
         elif (reply.startswith(":X195B4")) :
             event = canolcbutils.bodyArray(reply)
-	    if verbose : print "event produced", event
+            if verbose : print ("event produced", event)
+        elif (reply.startswith(":X198F4")) :
+            event = canolcbutils.bodyArray(reply)
+            if verbose : print ("Identify Consumer request ", event)
+        elif (reply.startswith(":X19914")) :
+            event = canolcbutils.bodyArray(reply)
+            if verbose : print ("Identify Producer request ", event)
         else :
-            print "Unexpected message"
-            return 50
+            print ("Unexpected message ", reply)
+            return (50)
     return 0
 
 if __name__ == '__main__':
