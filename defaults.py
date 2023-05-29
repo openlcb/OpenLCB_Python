1bce68d419f4e6c307651997cb0da2a49d1426b9
Bob Jacobsen <jacobsen@mac.com>
2023-05-29 14:46:48 -0400

    clean up

--- defaults.py
+++ defaults.py
@@ -1,40 +1,49 @@
-serial   = False
-tcp      = True
-ethernet = False
+# select the connection type (only one)
+serial   = True
 windows  = False
+serialPort = "/dev/cu.usbmodemCC570001B1"
+
+tcp      = False
+ethernet = False
+networkHost = "192.168.16.212"
+
 local    = False
 
+# thisNode is the node doing the testing
+thisNodeID = [1,2,3,4,5,6]
+thisNodeAlias = 0xAAA
 
-if tcp and not local:
+# testNode is the node under test
+# usually the -t option is used to gather this info from the single attached node
+testNodeID = [2,3,4,5,6,1]
+testNodeAlias = 0xDDD
+# an event produced and consumed by the device under test or None
+testEventID = [0x05, 0x02, 0x01, 0x02, 0x02, 0x00, 0x00, 0x00]
+
+
+if tcp :
     import tcpolcblink
     network = tcpolcblink.TcpToOlcbLink()
-    network.host = "174.18.137.234"
-    network.host = "localhost"
-    #network.host = "propername.local."
+    network.host = networkHost
     network.port = 12021
-elif ethernet and not local:
+elif ethernet :
     import ethernetolcblink
     network = ethernetolcblink.EthernetToOlcbLink()
-    network.host = "174.18.137.234"
-    #network.host = "propername.local."
+    network.host = networkHost
     network.port = 12021
-elif windows and not local :
+elif windows :
     import serialolcblink
     network = serialolcblink.SerialOlcbLink()
-    network.port = "COM9"
+    network.port = serialPort
     network.speed = 500000
     network.startdelay = 0
-elif serial and not local :
+elif serial :
     import serialolcblink
     network = serialolcblink.SerialOlcbLink()
-    #network.port = "/dev/cu.usbserial-A900fLVC"
-    #network.port = "/dev/cu.usbmodem401331"
-    #network.port = "/dev/cu.usbserial-AE015IZE" # Ioduino
-    #network.port = "/dev/cu.usbserial-A5VRG6OF" # TCH parallel
-    network.port = "/dev/cu.usbmodemCC570001B1" # RR-Cirkits
-    network.speed = 230400
-    network.parallel = True
-    network.startdelay = 0 # time to wait at start for adapter to come up, in seconds
+    network.port = serialPort
+    network.speed = 57600 #115200 #230400
+    network.parallel = False
+    network.startdelay = 1 # time to wait at start for adapter to come up, in seconds
 elif local :
     import pipeolcblink
     network = pipeolcblink.PipeOlcbLink()
@@ -43,10 +52,6 @@ else :
     print ("Please set one of the options to True")
 
 
-thisNodeID = [1,2,3,4,5,6]
-thisNodeAlias = 0xAAA
 testNodeID = [2,3,4,5,6,1]
 testNodeAlias = 0xDDD
-
-
 testEventID = [0x05, 0x02, 0x01, 0x02, 0x02, 0x00, 0x00, 0x00]
