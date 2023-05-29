c68d1047b238211b5a92d01f139cdb61987655ef
Bob Jacobsen <jacobsen@mac.com>
2023-05-29 09:26:24 -0400

    py3

--- testProducerConsumerNotification.py
+++ testProducerConsumerNotification.py
@@ -19,23 +19,23 @@ import identifyEventsAddressed
 import identifyConsumers
 import identifyProducers
 
-    
+
 def usage() :
-    print ""
-    print "Called standalone, will send one CAN IdentifyConsumers message"
-    print " and display response"
-    print ""
-    print "Expect zero or more ConsumerIdentified reply in return"
-    print "e.g. [1926Bsss] nn nn nn nn nn nn"
-    print "containing dest alias and EventID"
-    print ""
-    print "Default connection detail taken from connection.py"
-    print ""
-    print "-a --alias source alias (default 123)"
-    print "-d --dest dest alias (default 123)"
-    print "-t find destination alias automatically"
-    print "-v verbose"
-    print "-V very verbose"
+    print ("")
+    print ("Called standalone, will send one CAN IdentifyConsumers message")
+    print (" and display response")
+    print ("")
+    print ("Expect zero or more ConsumerIdentified reply in return")
+    print ("e.g. [1926Bsss] nn nn nn nn nn nn")
+    print ("containing dest alias and EventID")
+    print ("")
+    print ("Default connection detail taken from connection.py")
+    print ("")
+    print ("-a --alias source alias (default 123)")
+    print ("-d --dest dest alias (default 123)")
+    print ("-t find destination alias automatically")
+    print ("-v verbose")
+    print ("-V very verbose")
 
 import getopt, sys
 
@@ -44,13 +44,13 @@ def main():
     dest = connection.testNodeAlias;
     verbose = False
     identifynode = False
-    
+
     # argument processing
     try:
         opts, remainder = getopt.getopt(sys.argv[1:], "d:a:vVt", ["alias=", "dest="])
     except getopt.GetoptError, err:
         # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
+        print (str(err)) # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
     for opt, arg in opts:
@@ -75,7 +75,7 @@ def main():
     retval = test(alias, dest, connection, verbose)
     connection.network.close()
     return retval
-    
+
 def test(alias, dest, connection, verbose) :
     # now execute
     connection.network.send(identifyEventsAddressed.makeframe(alias, dest))
@@ -86,47 +86,49 @@ def test(alias, dest, connection, verbose) :
         if (reply == None ) : break
         if ( reply.startswith(":X194C7") or reply.startswith(":X194C4") or reply.startswith(":X194C5") ):
             event = canolcbutils.bodyArray(reply)
-            if verbose : print "  consumes ", event
+            if verbose : print ("  consumes "+str(event))
             consumed = consumed+[event]
         elif ( reply.startswith(":X19547") or reply.startswith(":X19544") or reply.startswith(":X19545") ):
             event = canolcbutils.bodyArray(reply)
-            if verbose : print "  produces ", event
+            if verbose : print ("  produces "+str(event))
             produced = produced+[event]
     # now check consumers and producers individually
     timeout = connection.network.timeout
     connection.network.timeout = 0.25
     if connection.network.verbose : print "Start individual checks"
     for c in consumed :
+        if verbose : print ("Check consumes "+str(c))
         connection.network.send(identifyConsumers.makeframe(alias, c))
         reply = connection.network.receive()
-        if (reply == None ) : 
-            print "no reply for consumer ", c
+        if (reply == None ) :
+            print ("no reply for consumer "+str(c))
             return 20
         elif not ( reply.startswith(":X194C7") or reply.startswith(":X194C4") or reply.startswith(":X194C5") ):
-            print "Unexpected reply "+reply
+            print ("Unexpected reply "+reply)
             return 21
         # here is OK, go around to next
         while True :
             reply = connection.network.receive()
             if (reply == None ) : break
             elif ( not reply.startswith(":X194C7") ) :
-                print "Unexpected reply "+reply
+                print ("Unexpected reply "+reply)
                 return 22
     for p in produced :
+        if verbose : print ("Check produces "+str(p))
         connection.network.send(identifyProducers.makeframe(alias, p))
         reply = connection.network.receive()
         if (reply == None ) :
-            print "no reply for producer ", p
+            print ("no reply for producer "+str(p))
             return 30
         elif not ( reply.startswith(":X19547") or reply.startswith(":X19544") or reply.startswith(":X19545") ):
-            print "Unexpected reply "+reply
+            print ("Unexpected reply "+reply)z
             return 31
         # here is OK, go around to next
         while True :
             reply = connection.network.receive()
             if (reply == None ) : break
             elif ( not reply.startswith(":X19547") ) :
-                print "Unexpected reply "+reply
+                print ("Unexpected reply "+reply)
                 return 32
     connection.network.timeout = timeout
     return 0
